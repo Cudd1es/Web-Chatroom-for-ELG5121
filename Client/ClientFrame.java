d1fcc25d89f5e4e6fadef48763439af22c6476ff
Amirhossein Hajianpour <ahhajianpour1@gmail.com>
2019-10-25 21:04:03 -0400

    Exception error added to nitification message.

--- Client/ClientFrame.java
+++ Client/ClientFrame.java
@@ -469,7 +469,7 @@ public class ClientFrame extends JFrame implements ActionListener {
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 			out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8")), true);
 		} catch (ConnectException e) {
-			JOptionPane.showMessageDialog(this, "Failed to connect to server!", "ERROR", JOptionPane.INFORMATION_MESSAGE);
+			JOptionPane.showMessageDialog(this, " Failed to connect to server! " + "Exception: " + e, "ERROR", JOptionPane.INFORMATION_MESSAGE);
 			txtServerIP.setText("");
 			System.out.println(e);
 		} catch (Exception e) {
