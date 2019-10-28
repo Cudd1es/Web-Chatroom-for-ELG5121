d0f699e72b901dd8d780b3b8bdfde99ae021b145
Cudd1es <j.ghost@icloud.com>
2019-10-28 11:14:19 -0400

    changed the space of texts

--- server/src/server/server_main_structure.java
+++ server/src/server/server_main_structure.java
@@ -5,23 +5,23 @@ import java.net.*;
 
 
 public class server_main_structure extends Thread {
-    ServerSocket serverSocket = null;
+    ServerSocket serverSocket = null; // 创建服务器端套接字
 
     public boolean bServerIsRunning = false;
-    private final int SERVER_PORT = 8888;
+    private final int SERVER_PORT = 8888;// 定义服务器端口号
 
     public server_main_structure() {
         try {
-            serverSocket = new ServerSocket(SERVER_PORT);
+            serverSocket = new ServerSocket(SERVER_PORT); // 启动服务
             bServerIsRunning = true;
 
             server_frame serverFrame = new server_frame();
-            getServerIP();
+            getServerIP(); // 得到并显示服务器端IP
             System.out.println("Server port is:" + SERVER_PORT);
             serverFrame.taLog.setText("Server started...");
             while (true) {
-                Socket socket = serverSocket.accept();
-                new server_process(socket, serverFrame);
+                Socket socket = serverSocket.accept(); // 监听客户端的连接请求，并返回客户端socket
+                new server_process(socket, serverFrame); // 创建一个新线程来处理与该客户的通讯
             }
         } catch (BindException e) {
             System.out.println("Port occupied....");
@@ -31,9 +31,10 @@ public class server_main_structure extends Thread {
             System.out.println("[ERROR] Could not start server." + e);
         }
 
-        this.start();
+        this.start(); // 启动线程
     }
 
+    // 获取服务器的主机名和IP地址
     public void getServerIP() {
         try {
             InetAddress serverAddress = InetAddress.getLocalHost();
@@ -49,15 +50,16 @@ public class server_main_structure extends Thread {
 
             System.out.println("Server IP is:" + s.getLocalAddress().getHostAddress());
             s.close();
-            //    System.out.println("Server IP is:" + (ipAddress[0] & 0xff) + "."
-            //           + (ipAddress[1] & 0xff) + "." + (ipAddress[2] & 0xff) + "."
-            //           + (ipAddress[3] & 0xff));
+         //    System.out.println("Server IP is:" + (ipAddress[0] & 0xff) + "."
+         //           + (ipAddress[1] & 0xff) + "." + (ipAddress[2] & 0xff) + "."
+         //           + (ipAddress[3] & 0xff));
         } catch (Exception e) {
             System.out.println("###Cound not get Server IP." + e);
         }
     }
 
+    // main方法，实例化服务器端程序
     public static void main(String args[]) {
-        new server_main_structure();
+       new server_main_structure();
     }
 }
